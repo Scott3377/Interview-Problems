7481bad9b79050b68ba36d600bb091789635ee2d
Scott3377 <Scott3377@hotmail.com>
2013-06-03 16:02:07 -0500

    Fix comments formatting

--- Longest_Word_Submission(Github).rb
+++ Longest_Word_Submission(Github).rb
@@ -14,11 +14,11 @@
 # CONSTANT
 WORD_FILE = "wordlist.txt"
 
-word_combo_list = []									# Holds list of concatenated words
-$word_hash = Hash.new()									# Hash containing words in list as key, array of inner words as values
-$key_length = 0											# Cache variable for key length
+word_combo_list = []							# Holds list of concatenated words
+$word_hash = Hash.new()							# Hash containing words in list as key, array of inner words as values
+$key_length = 0								# Cache variable for key length
 
-File.open(WORD_FILE, "r") do |f|						# Iterate through file and add words to hash
+File.open(WORD_FILE, "r") do |f|					# Iterate through file and add words to hash
 	f.each_line do |word|
 		word.chomp!
 		if !word.empty?
@@ -29,33 +29,33 @@ end
 
 # Method that loops through word looking for word combinations
 def check_for_word(word, snippet = "", rest_of_word = "")
-	if rest_of_word.empty?								# If initial run, rest_of_word = original word
+	if rest_of_word.empty?						# If initial run, rest_of_word = original word
 		rest_of_word = word
 	end
 
 	rest_of_word.split("").each do |char|				# Look for words within latter part of string
-		snippet += char									# Append next character from rest_of_string to snippet
-		if $word_hash.has_key?(snippet)					# If inner word found in list of words
-			$word_hash[word] << snippet					# Append snippet to found words array
-			snippet = ""								# Reset snippet
+		snippet += char						# Append next character from rest_of_string to snippet
+		if $word_hash.has_key?(snippet)				# If inner word found in list of words
+			$word_hash[word] << snippet			# Append snippet to found words array
+			snippet = ""					# Reset snippet
 		end
 	end
 	
-	if !snippet.empty?									# No clean combo. Try for longer root words
+	if !snippet.empty?						# No clean combo. Try for longer root words
 		rest_of_word = word.dup
 		rest_of_word.slice! $word_hash[word].join		# rest of word = original word without found words
-		snippet = $word_hash[word].pop()				# snippet = last found word
+		snippet = $word_hash[word].pop()			# snippet = last found word
 		check_for_word(word, snippet, rest_of_word)		# Call method again and look for longer words
 	end
-	
-	return $word_hash[word].last.length != $key_length	# If word combo was pure combo of other words (& not word itself)
+
+	return $word_hash[word].last.length != $key_length		# If word combo was pure combo of other words (& not word itself)
 end
 
 # MAIN PROGRAM
-$word_hash.keys.each do |key|							# Loop through list of words in list (keys in hash)
-	$key_length = key.length							# Cache key length
+$word_hash.keys.each do |key|						# Loop through list of words in list (keys in hash)
+	$key_length = key.length					# Cache key length
 	no_extra_letters = check_for_word(key)				# Look for clean word combo (no extra letters left over)
-	if no_extra_letters									# If clean combo and has more matches than just the original word, append to combo list
+	if no_extra_letters						# If clean combo and has more matches than just the original word, append to combo list
 		word_combo_list << key
 	end
 end
