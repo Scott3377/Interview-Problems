f9e87fb82ef430da372650c6464ab14cd57412fd
Scott Thomas <Scott3377@hotmail.com>
2013-06-04 15:29:29 -0500

    Added more illustrations to initial comments.
    Added how the recursive word finding solution works along with
    illustrations of how arrays of words are accumulated.

--- Longest_Word_Submission(Github).rb
+++ Longest_Word_Submission(Github).rb
@@ -8,7 +8,40 @@
 # if it was found as a word from the list and added to the array of words found for that word.  If a clean combination
 # was not found, I would go back and look for longer initial words. Every hash would have a minimum of one match from the word list (the word itself)
 # or a clean combination of words (no unmatched characters left over), but only clean combinations would be added to the word_combo_list.
-
+#
+# Here are some examples of how this solution is an improvement over a more naive solution that merely finds only the first word. within a word
+# 
+# WRONG: word_hash["catchall"] => ["cat"] + "chall"
+# 
+# 			vs.
+# 
+# CORRECT: word_hash["catchall"] => ["catch","all"]
+#
+# The proper solution requires a recursive function that calls itself and look for longer prior words. See below for how it works:
+# 
+# :: CALL check_for_word() FUNCTION ::
+# 
+# word_hash["catchall"] => "c" + "atchall"
+# word_hash["catchall"] => "ca" + "tchall"
+# word_hash["catchall"] => ["cat"] + "chall"
+# word_hash["catchall"] => ["cat"] + "c" + "hall"
+# word_hash["catchall"] => ["cat"] + "ch" + "all"
+# word_hash["catchall"] => ["cat"] + "cha" + "ll"
+# word_hash["catchall"] => ["cat"] + "chal" + "l"
+# word_hash["catchall"] => ["cat"] + "chall"
+#
+# :: NO WORD FOUND ::
+# :: CALL check_for_word() FUNCTION again(popping the last array element off of the stack & passing in the index of the last found word) ::
+# 
+# word_hash["catchall"] => "catc" + "hall"
+# word_hash["catchall"] => ["catch"] + "all"
+# word_hash["catchall"] => ["catch"] + "a" + "ll"
+# word_hash["catchall"] => ["catch"] + "al" + "l"
+# word_hash["catchall"] => ["catch","all"]
+# 
+# :: BOTH WORDS FOUND ::
+#
+#
 # NOTE: Requires "wordlist.txt" in same directory as program code.
 
 # CONSTANT
